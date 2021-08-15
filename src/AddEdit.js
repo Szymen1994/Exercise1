0a84a91fdd3e26e5f04292c8da7cfec47af04fad
Szymen1994 <61362970+Szymen1994@users.noreply.github.com>
2021-08-15 12:06:36 +0200

    dodano even listener do textboxow

--- src/AddEdit.js
+++ src/AddEdit.js
@@ -1,16 +1,28 @@
 import React from 'react';
 import './style.css';
 
+<script src="https://gist.github.com/Szymen1994/940609d33b9008884fd37db63a22b23a.js"></script>
+
 export default function AddEdit(){
+    //wyszukiwanie gistów
+    const Nazwa = document.getElementById('nazwa');
+    const Zawartosc = document.getElementById('zawartość');
+    const Tytul = document.getElementById('tytuł');
+
+    Nazwa.addEventListener('keyup', (e) => {
+        console.log(e.target.value)
+    })
+
+    Zawartosc.addEventListener('keyup', (i) => {
+        console.log(i.target.value)
+    })
+
+    Tytul.addEventListener('keyup', (u) => {
+        console.log(u.target.value)
+    })
+
     return(
-    <>
-        <form action="">
-            <input type="text" name="nazwa" placeholder="Nazwa pliku"/>       {/*nazwa pliku*/}
-            <input type="text" name="zawartość" placeholder="Zawartość"/>   {/*zawartość Gist'a*/}
-            <input type="text" name="tytuł" placeholder="Tytuł"/>       {/*Tytuł Gista*/}
-            <button>Dodaj Gist</button>
-            <button>Edytuj Gist</button>
-        </form>
-    </>
+        <>
+            </>
     )
 }
--- src/App.js
+++ src/App.js
@@ -5,18 +5,52 @@ import './style.css';
 function App() {
   return (
     <>
-      <header>
-        <h2>Gists Search</h2>
-      </header>
+      <div>
+        
+        <header>
+          <h2>Gists Search</h2>
+        </header>
+        
+        <form>
 
-      <AddEdit />
-      <button> Usuń Gist </button>      {/*akcja*/}
-      <button> Listuj Gisty </button>   {/*Paginacja*/}
-      <button> Filtruj Gisty </button>  {/*Paginacja-Filtrowanie*/}
-      {/*lista Gistów*/}
-      <form>
+          {/*nazwa pliku*/}
+          <input 
+            type="text" 
+            name="nazwa" 
+            placeholder="Nazwa pliku" 
+            id="nazwa" 
+          />
 
-      </form>
+          {/*zawartość Gist'a*/}  
+          <input 
+            type="text" 
+            name="zawartość" 
+            placeholder="Zawartość" 
+            id="zawartość" 
+          />
+
+          {/*Tytuł Gista*/}
+          <input 
+            type="text" 
+            name="tytuł" 
+            placeholder="Tytuł" 
+            id="tytuł"
+          />       
+            
+          <button onclick="dodaj()">Dodaj Gist</button>
+          <button onclick="edit()">Edytuj Gist</button>
+          {/*Lista Gistów*/}
+          <div id="lista"></div>
+
+        </form>
+        
+        <AddEdit />
+        <button> Usuń Gist </button>      {/*akcja*/}
+        <button> Listuj Gisty </button>   {/*Paginacja*/}
+        <button> Filtruj Gisty </button>  {/*Paginacja-Filtrowanie*/}
+      </div>
+      
+      <ul id="gistsList"></ul>          {/*lista Gistów*/}
     </>
   )
 }
