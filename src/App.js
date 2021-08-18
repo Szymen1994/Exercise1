20410a1990da15502cc03f30826333db01ca6f69
Szymen1994 <61362970+Szymen1994@users.noreply.github.com>
2021-08-18 22:22:19 +0200

    wersja 2 dodano funkcjonalnosc i poprawiono css

--- src/App.js
+++ src/App.js
@@ -1,58 +1,26 @@
 import React from 'react';
-import AddEdit from './AddEdit';
+import Buttons from './Buttons.js';
 import './style.css';
+import Form from './Form.js';
 
 function App() {
   return (
     <>
-      <div>
+      
         
-        <header>
+        <nav>
           <h2>Gists Search</h2>
-        </header>
+        </nav>
+          
+        <Form />
+        <Buttons />
         
-        <form>
-
-          {/*nazwa pliku*/}
-          <input 
-            type="text" 
-            name="nazwa" 
-            placeholder="Nazwa pliku" 
-            id="nazwa" 
-          />
-
-          {/*zawartość Gist'a*/}  
-          <input 
-            type="text" 
-            name="zawartość" 
-            placeholder="Zawartość" 
-            id="zawartość" 
-          />
-
-          {/*Tytuł Gista*/}
-          <input 
-            type="text" 
-            name="tytuł" 
-            placeholder="Tytuł" 
-            id="tytuł"
-          />       
-            
-          <button onclick="dodaj()">Dodaj Gist</button>
-          <button onclick="edit()">Edytuj Gist</button>
-          {/*Lista Gistów*/}
-          <div id="lista"></div>
-
-        </form>
-        
-        <AddEdit />
-        <button> Usuń Gist </button>      {/*akcja*/}
-        <button> Listuj Gisty </button>   {/*Paginacja*/}
-        <button> Filtruj Gisty </button>  {/*Paginacja-Filtrowanie*/}
-      </div>
+      
       
       <ul id="gistsList"></ul>          {/*lista Gistów*/}
     </>
   )
 }
+
 
 export default App;
